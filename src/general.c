da801a6a06cb4232da9ec6a165ad98006e3fceb8
Miguel de Icaza <miguel@gnome.org>
2004-09-27 20:38:39 +0000

    2004-09-27  Mart Roosmaa <roosmaa@gmail.com>
    	* general.c: Fallback to 72 dpis if we can not determine this
    	information.
    
    svn path=/branches/mono-1-0/libgdiplus/; revision=34449

--- src/general.c
+++ src/general.c
@@ -175,7 +175,9 @@ gdip_get_display_dpi()
             char *val = XGetDefault(GDIP_display, "Xft", "dpi");
             if (val) {
                 dpis = atof(val);
-            }
+            } else {
+		dpis = 72.0f;
+	    }
         }
     }
 
