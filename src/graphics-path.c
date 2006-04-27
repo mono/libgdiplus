19ace1b65d070d1df828a7e2622b42db1f058362
Sebastien Pouliot <sebastien@ximian.com>
2006-04-27 14:36:37 +0000

    fix compilation warning (turned into an error) with gcc 4.x
    svn path=/trunk/libgdiplus/; revision=59983

--- src/graphics-path.c
+++ src/graphics-path.c
@@ -1015,7 +1015,7 @@ g_warning ("GdipAddString \"%s\" (family: %s, size %g)", utf8, ucs2_to_utf8 ((co
 	/* TODO - deal with layoutRect, format... ideally we would be calling a subset
 	   of GdipDrawString that already does everything *and* preserve the whole path */
 	cairo_move_to (cr, layoutRect->X, layoutRect->Y + font->sizeInPixels);
-	cairo_text_path (cr, utf8);
+	cairo_text_path (cr, (const char*)utf8);
 
 	/* get the font data from the cairo path and translate it as a gdi+ path */
 	cp = cairo_copy_path (cr);
