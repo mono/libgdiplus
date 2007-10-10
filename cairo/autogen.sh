1926cd5dc4beab612cac459de63854640dd23550
Geoff Norton <grompf@sublimeintervention.com>
2007-10-10 01:20:22 +0000

    2007-10-09  Geoff Norton  <gnorton@novell.com>
    	* autogen.sh: Backport autogen.sh from newer cairo to support automake 1.10+
    
    svn path=/trunk/libgdiplus/; revision=87236

--- cairo/autogen.sh
+++ cairo/autogen.sh
@@ -2,26 +2,6 @@
 # Run this to generate all the initial makefiles, etc.
 set -e
 
-PACKAGE=cairo
-
-LIBTOOLIZE=${LIBTOOLIZE-libtoolize}
-LIBTOOLIZE_FLAGS="--copy --force"
-AUTOHEADER=${AUTOHEADER-autoheader}
-AUTOMAKE_FLAGS="--add-missing"
-AUTOCONF=${AUTOCONF-autoconf}
-
-# automake 1.8 requires autoconf 2.58
-# automake 1.7 requires autoconf 2.54
-automake_min_vers=1.7
-aclocal_min_vers=$automake_min_vers
-autoconf_min_vers=2.54
-libtoolize_min_vers=1.4
-
-# The awk-based string->number conversion we use needs a C locale to work 
-# as expected. Setting LC_ALL overrides whether the user set LC_ALL,
-# LC_NUMERIC, or LANG.
-LC_ALL=C
-
 ARGV0=$0
 
 # Allow invocation from a separate build directory; in that case, we change
@@ -30,25 +10,126 @@ srcdir=`dirname $ARGV0`
 test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
-
 cd $srcdir
 
-if ($AUTOCONF --version) < /dev/null > /dev/null 2>&1 ; then
-    if ($AUTOCONF --version | head -n 1 | awk 'NR==1 { if( $(NF) >= '$autoconf_min_vers') \
-			       exit 1; exit 0; }');
-    then
-       echo "$ARGV0: ERROR: \`$AUTOCONF' is too old."
-       $AUTOCONF --version
-       echo "           (version $autoconf_min_vers or newer is required)"
-       DIE="yes"
-    fi
-else
-    echo $AUTOCONF: command not found
-    echo
-    echo "$ARGV0: ERROR: You must have \`autoconf' installed to compile $PACKAGE."
-    echo "           (version $autoconf_min_vers or newer is required)"
-    DIE="yes"
+PACKAGE=cairo
+
+LIBTOOLIZE=${LIBTOOLIZE-libtoolize}
+LIBTOOLIZE_FLAGS="--copy --force"
+AUTOHEADER=${AUTOHEADER-autoheader}
+AUTOMAKE_FLAGS="--add-missing --foreign"
+AUTOCONF=${AUTOCONF-autoconf}
+
+CONFIGURE_IN=
+test -f configure.in && CONFIGURE_IN=configure.in
+test -f configure.ac && CONFIGURE_IN=configure.ac
+
+if test "X$CONFIGURE_IN" = X; then
+  echo "$ARGV0: ERROR: No $srcdir/configure.in or $srcdir/configure.ac found."
+  exit 1
 fi
+
+extract_version() {
+	grep "^ *$1" $CONFIGURE_IN | sed 's/.*(\[*\([^])]*\)]*).*/\1/'
+}
+
+autoconf_min_vers=`extract_version AC_PREREQ`
+automake_min_vers=`extract_version AM_INIT_AUTOMAKE`
+libtoolize_min_vers=`extract_version AC_PROG_LIBTOOL`
+aclocal_min_vers=$automake_min_vers
+
+
+# Not all echo versions allow -n, so we check what is possible. This test is
+# based on the one in autoconf.
+case `echo "testing\c"; echo 1,2,3`,`echo -n testing; echo 1,2,3` in
+  *c*,-n*) ECHO_N= ;;
+  *c*,*  ) ECHO_N=-n ;;
+  *)       ECHO_N= ;;
+esac
+
+
+# some terminal codes ...
+boldface="`tput bold 2>/dev/null`"
+normal="`tput sgr0 2>/dev/null`"
+printbold() {
+    echo $ECHO_N "$boldface"
+    echo "$@"
+    echo $ECHO_N "$normal"
+}
+printerr() {
+    echo "$@" >&2
+}
+
+
+# Usage:
+#     compare_versions MIN_VERSION ACTUAL_VERSION
+# returns true if ACTUAL_VERSION >= MIN_VERSION
+compare_versions() {
+    ch_min_version=$1
+    ch_actual_version=$2
+    ch_status=0
+    IFS="${IFS=         }"; ch_save_IFS="$IFS"; IFS="."
+    set $ch_actual_version
+    for ch_min in $ch_min_version; do
+        ch_cur=`echo $1 | sed 's/[^0-9].*$//'`; shift # remove letter suffixes
+        if [ -z "$ch_min" ]; then break; fi
+        if [ -z "$ch_cur" ]; then ch_status=1; break; fi
+        if [ $ch_cur -gt $ch_min ]; then break; fi
+        if [ $ch_cur -lt $ch_min ]; then ch_status=1; break; fi
+    done
+    IFS="$ch_save_IFS"
+    return $ch_status
+}
+
+# Usage:
+#     version_check PACKAGE VARIABLE CHECKPROGS MIN_VERSION SOURCE
+# checks to see if the package is available
+version_check() {
+    vc_package=$1
+    vc_variable=$2
+    vc_checkprogs=$3
+    vc_min_version=$4
+    vc_source=$5
+    vc_status=1
+
+    vc_checkprog=`eval echo "\\$$vc_variable"`
+    if [ -n "$vc_checkprog" ]; then
+	printbold "using $vc_checkprog for $vc_package"
+	return 0
+    fi
+
+    printbold "checking for $vc_package >= $vc_min_version..."
+    for vc_checkprog in $vc_checkprogs; do
+	echo $ECHO_N "  testing $vc_checkprog... "
+	if $vc_checkprog --version < /dev/null > /dev/null 2>&1; then
+	    vc_actual_version=`$vc_checkprog --version | head -n 1 | \
+                               sed 's/^.*[ 	]\([0-9.]*[a-z]*\).*$/\1/'`
+	    if compare_versions $vc_min_version $vc_actual_version; then
+		echo "found $vc_actual_version"
+		# set variable
+		eval "$vc_variable=$vc_checkprog"
+		vc_status=0
+		break
+	    else
+		echo "too old (found version $vc_actual_version)"
+	    fi
+	else
+	    echo "not found."
+	fi
+    done
+    if [ "$vc_status" != 0 ]; then
+	printerr "***Error***: You must have $vc_package >= $vc_min_version installed"
+	printerr "  to build $PROJECT.  Download the appropriate package for"
+	printerr "  from your distribution or get the source tarball at"
+        printerr "    $vc_source"
+	printerr
+    fi
+    return $vc_status
+}
+
+
+version_check autoconf AUTOCONF $AUTOCONF $autoconf_min_vers \
+    "http://ftp.gnu.org/pub/gnu/autoconf/autoconf-${autoconf_min_vers}.tar.gz" || DIE=1
 
 #
 # Hunt for an appropriate version of automake and aclocal; we can't
@@ -56,75 +137,13 @@ fi
 # 
 # We check automake first to allow it to be a newer version than we know about.
 #
-if test x"$AUTOMAKE" = x || test x"$ACLOCAL" = x ; then
-  am_ver=""
-  for ver in "" "-1.9" "-1.8" "-1.7" ; do
-    am="automake$ver"
-    if ($am --version) < /dev/null > /dev/null 2>&1 ; then
-      if ($am --version | head -n 1 | awk 'NR==1 { if( $(NF) >= '$automake_min_vers') \
-	  		 exit 1; exit 0; }'); then : ; else
-         am_ver=$ver
-         break;
-      fi
-    fi
-  done
-  
-  AUTOMAKE=${AUTOMAKE-automake$am_ver}
-  ACLOCAL=${ACLOCAL-aclocal$am_ver}
-fi
+version_check automake AUTOMAKE "$AUTOMAKE automake automake-1.10 automake-1.9 automake-1.8 automake-1.7" $automake_min_vers \
+    "http://ftp.gnu.org/pub/gnu/automake/automake-${automake_min_vers}.tar.gz" || DIE=1
+ACLOCAL=`echo $AUTOMAKE | sed s/automake/aclocal/`
 
-#
-# Now repeat the tests with the copies we decided upon and error out if they
-# aren't sufficiently new.
-#
-if ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 ; then
-  if ($AUTOMAKE --version | head -n 1 | awk 'NR==1 { if( $(NF) >= '$automake_min_vers') \
-			     exit 1; exit 0; }');
-     then
-     echo "$ARGV0: ERROR: \`$AUTOMAKE' is too old."
-     $AUTOMAKE --version
-     echo "           (version $automake_min_vers or newer is required)"
-     DIE="yes"
-  fi
-  if ($ACLOCAL --version) < /dev/null > /dev/null 2>&1; then
-    if ($ACLOCAL --version | head -n 1 | awk 'NR==1 { if( $(NF) >= '$aclocal_min_vers' ) \
-						exit 1; exit 0; }' );
-    then
-      echo "$ARGV0: ERROR: \`$ACLOCAL' is too old."
-      $ACLOCAL --version
-      echo "           (version $aclocal_min_vers or newer is required)"
-      DIE="yes"
-    fi
-  else
-    echo $ACLOCAL: command not found
-    echo
-    echo "$ARGV0: ERROR: Missing \`$ACLOCAL'"
-    echo "           The version of $AUTOMAKE installed doesn't appear recent enough."
-    DIE="yes"
-  fi
-else
-    echo $AUTOMAKE: command not found
-    echo
-    echo "$ARGV0: ERROR: You must have \`automake' installed to compile $PACKAGE."
-    echo "           (version $automake_min_vers or newer is required)"
-    DIE="yes"
-fi
 
-if ($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 ; then
-    if ($LIBTOOLIZE --version | awk 'NR==1 { if( $4 >= '$libtoolize_min_vers') \
-			       exit 1; exit 0; }');
-    then
-       echo "$ARGV0: ERROR: \`$LIBTOOLIZE' is too old."
-       echo "           (version $libtoolize_min_vers or newer is required)"
-       DIE="yes"
-    fi
-else
-    echo $LIBTOOLIZE: command not found
-    echo
-    echo "$ARGV0: ERROR: You must have \`libtoolize' installed to compile $PACKAGE."
-    echo "           (version $libtoolize_min_vers or newer is required)"
-    DIE="yes"
-fi
+version_check libtool LIBTOOLIZE $LIBTOOLIZE $libtoolize_min_vers \
+    "http://ftp.gnu.org/pub/gnu/libtool/libtool-${libtool_min_vers}.tar.gz" || DIE=1
 
 if test -z "$ACLOCAL_FLAGS"; then
     acdir=`$ACLOCAL --print-ac-dir`
@@ -170,4 +189,11 @@ do_cmd $AUTOCONF
 
 cd $ORIGDIR || exit 1
 
-do_cmd $srcdir/configure --enable-maintainer-mode --enable-gtk-doc ${1+"$@"} && echo "Now type \`make' to compile" || exit 1
+rm -f config.cache
+
+do_cmd $srcdir/configure \
+	--cache-file=config.cache \
+	--disable-static \
+	--enable-gtk-doc \
+	--enable-test-surfaces \
+	${1+"$@"} && echo "Now type \`make' to compile $PROJECT." || exit 1
