20598dd3b08d764966107f18849117df8bb03768
Sebastien Pouliot <sebastien@ximian.com>
2010-07-07 21:15:21 +0000

    2010-07-07  Sebastien Pouliot  <sebastien@ximian.com>
    	* pngcodec.c: security fix in libpng 1.2 broke the define
    	[Backport r160026]
    
    
    svn path=/branches/mono-2-6/libgdiplus/; revision=160027

--- src/pngcodec.c
+++ src/pngcodec.c
@@ -352,7 +352,7 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 						info_ptr->palette[i].blue,
 						info_ptr->palette[i].green,
 						info_ptr->palette[i].red,
-#if PNG_LIBPNG_VER > 10243
+#if PNG_LIBPNG_VER > 10399
 						info_ptr->trans_alpha [i]); /* alpha */
 #else
 						info_ptr->trans[i]); /* alpha */
@@ -422,7 +422,7 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 		}
 
 		if ((color_type == PNG_COLOR_TYPE_GRAY) && (bit_depth < 8)) {
-#if PNG_LIBPNG_VER > 10243
+#if PNG_LIBPNG_VER > 10399
 			png_set_expand_gray_1_2_4_to_8 (png_ptr);
 #else
 			png_set_gray_1_2_4_to_8(png_ptr);
