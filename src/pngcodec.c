mono/libgdiplus

commit 5ed200d5d73edc54969b34c708d18728d38c070b

Add a new table to speed up alpha premultiplication and fix bugs #79297 and #80366
svn path=/trunk/libgdiplus/; revision=70434

src/pngcodec.c
@@ -32,6 +32,7 @@
 #ifdef HAVE_LIBPNG
 
 #include <png.h>
+#include "general.h"
 #include "pngcodec.h"
 
 
@@ -435,7 +436,22 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 				for (i = 0; i < height; i++) {
 					png_bytep rowp = row_pointers[i];
 					for (j = 0; j < width; j++) {
-						set_pixel_bgra (rawptr, 0, rowp[2], rowp[1], rowp[0], rowp[3]);
+						byte a = rowp[3];
+						if (a == 0) {
+							set_pixel_bgra (rawptr, 0, 0, 0, 0, 0);
+						} else {
+							byte b = rowp[2];
+							byte g = rowp[1];
+							byte r = rowp[0];
+
+							if (a < 0xff) {
+								r = pre_multiplied_table [r][a];
+								g = pre_multiplied_table [g][a];
+								b = pre_multiplied_table [b][a];
+							}
+
+							set_pixel_bgra (rawptr, 0, b, g, r, a);
+						}
 						rowp += 4;
 						rawptr += 4;
 					}
@@ -493,6 +509,10 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 			result->active_bitmap->pixel_format = Format8bppIndexed;
 			result->active_bitmap->image_flags = ImageFlagsColorSpaceGRAY;
 		}
+
+		if (color_type & PNG_COLOR_MASK_ALPHA)
+			 result->active_bitmap->image_flags |= ImageFlagsHasAlpha;
+
 		result->active_bitmap->image_flags |= ImageFlagsReadOnly | ImageFlagsHasRealPixelSize;
 		result->active_bitmap->dpi_horz = 0;
 		result->active_bitmap->dpi_vert = 0;

src/general.c
@@ -48,6 +48,8 @@ struct startupOutput
 	void *unhook;
 };
 
+/* large table to avoid a division and three multiplications when premultiplying alpha into R, G and B */
+#include "alpha-premul-table.inc"
 
 static GList* g_mem_allocations;
 static BOOL startup = FALSE;

src/general.h
@@ -42,6 +42,8 @@
 #define gdip_convgr_unitx(g,f)		(gdip_unit_conversion (UnitCairoPoint, g->page_unit, g->dpi_x, g->type, f))
 #define gdip_convgr_unity(g,f)		(gdip_unit_conversion (UnitCairoPoint, g->page_unit, g->dpi_y, g->type, f))
 
+/* avoid floating point division/multiplications when pre-multiplying the alpha channel with R, G and B values */
+const byte pre_multiplied_table[256][256];
 
 /* cairo has a (signed) 15(1)/16(2)bits pixel positioning, while GDI+ use (signed) 23 bits (infinity).
  * Using larger values confuse the bits used for subpixel positioning.

src/imageattributes.c
@@ -21,7 +21,7 @@
  *
  */
 
-#include "gdip.h"
+#include "general.h"
 #include <math.h>
 
 void
@@ -224,20 +224,19 @@ gdip_process_bitmap_attributes (GpBitmap *bitmap, void **dest, GpImageAttributes
 					g_new = (r * cm->m[0][1] + g * cm->m[1][1] + b * cm->m[2][1] + a * cm->m[3][1] + (255 * cm->m[4][1]));
 					b_new = (r * cm->m[0][2] + g * cm->m[1][2] + b * cm->m[2][2] + a * cm->m[3][2] + (255 * cm->m[4][2]));
 
-					/* remember that Cairo use pre-multiplied alpha, e.g. 50% red == 0x80800000 not 0x80ff0000 */
-					if (a_new < 0xff) {
-						float alpha_pre_mul = ((float) a_new) / 0xff;
-						r_new = (byte) (r_new * alpha_pre_mul);
-						g_new = (byte) (g_new * alpha_pre_mul);
-						b_new = (byte) (b_new * alpha_pre_mul);
-						a = (byte) a_new;
-					} else {
-						a = 0xff;
-					}
-
 					r = (r_new > 0xff) ? 0xff : (byte) r_new;
 					g = (g_new > 0xff) ? 0xff : (byte) g_new;
 					b = (b_new > 0xff) ? 0xff : (byte) b_new;
+
+					/* remember that Cairo use pre-multiplied alpha, e.g. 50% red == 0x80800000 not 0x80ff0000 */
+					if (a_new < 0xff) {
+						a = (byte) a_new;
+						r = pre_multiplied_table [r][a];
+						g = pre_multiplied_table [g][a];
+						b = pre_multiplied_table [b][a];
+					} else {
+						a = 0xff;
+					}
 
 					set_pixel_bgra (color_p, 0, b, g, r, a);
 					*scan++ = color;

commit 96ee61fa69df42567864b8d8241626d6cf6082a3

Do not zero RGB pixels if A == 0
This fixes a problem introduced by
5ed200d5d73edc54969b34c708d18728d38c070b.

src/pngcodec.c
@@ -465,16 +465,12 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 				for (i = 0; i < height; i++) {
 					png_bytep rowp = row_pointers[i];
 					for (j = 0; j < width; j++) {
+						BYTE b = rowp[2];
+						BYTE g = rowp[1];
+						BYTE r = rowp[0];
 						BYTE a = rowp[3];
-						if (a == 0) {
-							set_pixel_bgra (rawptr, 0, 0, 0, 0, 0);
-						} else {
-							BYTE b = rowp[2];
-							BYTE g = rowp[1];
-							BYTE r = rowp[0];
 
-							set_pixel_bgra (rawptr, 0, b, g, r, a);
-						}
+						set_pixel_bgra (rawptr, 0, b, g, r, a);
 						rowp += 4;
 						rawptr += 4;
 					}