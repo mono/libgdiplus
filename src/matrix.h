8a14606c342209ba935201a3b0dc5b982280f943
Hugh Bellamy <hughbellars@gmail.com>
2017-06-28 12:27:21 +0700

    Fix Matrix Window exports

--- src/matrix.h
+++ src/matrix.h
@@ -23,26 +23,26 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
 
-GpStatus GdipCreateMatrix (GpMatrix **matrix);
-GpStatus GdipCreateMatrix2 (float m11, float m12, float m21, float m22, float dx, float dy, GpMatrix **matrix);
-GpStatus GdipCreateMatrix3 (GDIPCONST GpRectF *rect, GDIPCONST GpPointF *dstplg, GpMatrix **matrix);
-GpStatus GdipCreateMatrix3I (GDIPCONST GpRect *rect, GDIPCONST GpPoint *dstplg, GpMatrix **matrix);
-GpStatus GdipCloneMatrix (GpMatrix *matrix, GpMatrix **cloneMatrix);
-GpStatus GdipDeleteMatrix (GpMatrix *matrix);
-GpStatus GdipSetMatrixElements (GpMatrix *matrix, float m11, float m12, float m21, float m22, float dx, float dy);
-GpStatus GdipMultiplyMatrix (GpMatrix *matrix, GpMatrix *matrix2, GpMatrixOrder order);
-GpStatus GdipTranslateMatrix (GpMatrix *matrix, float offsetX, float offsetY, GpMatrixOrder order);
-GpStatus GdipScaleMatrix (GpMatrix *matrix, float scaleX, float scaleY, GpMatrixOrder order);
-GpStatus GdipRotateMatrix (GpMatrix *matrix, float angle, GpMatrixOrder order);
-GpStatus GdipShearMatrix (GpMatrix *matrix, float shearX, float shearY, GpMatrixOrder order);
-GpStatus GdipInvertMatrix (GpMatrix *matrix);
-GpStatus GdipTransformMatrixPoints (GpMatrix *matrix, GpPointF *pts, int count);
-GpStatus GdipTransformMatrixPointsI (GpMatrix *matrix, GpPoint *pts, int count);
-GpStatus GdipVectorTransformMatrixPoints (GpMatrix *matrix, GpPointF *pts, int count);
-GpStatus GdipVectorTransformMatrixPointsI (GpMatrix *matrix, GpPoint *pts, int count);
-GpStatus GdipGetMatrixElements (GpMatrix *matrix, float *matrixOut);
-GpStatus GdipIsMatrixInvertible (GpMatrix *matrix, BOOL *result);
-GpStatus GdipIsMatrixIdentity (GpMatrix *matrix, BOOL *result);
-GpStatus GdipIsMatrixEqual (GpMatrix *matrix, GpMatrix *matrix2, BOOL *result);
+GpStatus WINGDIPAPI GdipCreateMatrix (GpMatrix **matrix);
+GpStatus WINGDIPAPI GdipCreateMatrix2 (float m11, float m12, float m21, float m22, float dx, float dy, GpMatrix **matrix);
+GpStatus WINGDIPAPI GdipCreateMatrix3 (GDIPCONST GpRectF *rect, GDIPCONST GpPointF *dstplg, GpMatrix **matrix);
+GpStatus WINGDIPAPI GdipCreateMatrix3I (GDIPCONST GpRect *rect, GDIPCONST GpPoint *dstplg, GpMatrix **matrix);
+GpStatus WINGDIPAPI GdipCloneMatrix (GpMatrix *matrix, GpMatrix **cloneMatrix);
+GpStatus WINGDIPAPI GdipDeleteMatrix (GpMatrix *matrix);
+GpStatus WINGDIPAPI GdipSetMatrixElements (GpMatrix *matrix, float m11, float m12, float m21, float m22, float dx, float dy);
+GpStatus WINGDIPAPI GdipMultiplyMatrix (GpMatrix *matrix, GpMatrix *matrix2, GpMatrixOrder order);
+GpStatus WINGDIPAPI GdipTranslateMatrix (GpMatrix *matrix, float offsetX, float offsetY, GpMatrixOrder order);
+GpStatus WINGDIPAPI GdipScaleMatrix (GpMatrix *matrix, float scaleX, float scaleY, GpMatrixOrder order);
+GpStatus WINGDIPAPI GdipRotateMatrix (GpMatrix *matrix, float angle, GpMatrixOrder order);
+GpStatus WINGDIPAPI GdipShearMatrix (GpMatrix *matrix, float shearX, float shearY, GpMatrixOrder order);
+GpStatus WINGDIPAPI GdipInvertMatrix (GpMatrix *matrix);
+GpStatus WINGDIPAPI GdipTransformMatrixPoints (GpMatrix *matrix, GpPointF *pts, int count);
+GpStatus WINGDIPAPI GdipTransformMatrixPointsI (GpMatrix *matrix, GpPoint *pts, int count);
+GpStatus WINGDIPAPI GdipVectorTransformMatrixPoints (GpMatrix *matrix, GpPointF *pts, int count);
+GpStatus WINGDIPAPI GdipVectorTransformMatrixPointsI (GpMatrix *matrix, GpPoint *pts, int count);
+GpStatus WINGDIPAPI GdipGetMatrixElements (GpMatrix *matrix, float *matrixOut);
+GpStatus WINGDIPAPI GdipIsMatrixInvertible (GpMatrix *matrix, BOOL *result);
+GpStatus WINGDIPAPI GdipIsMatrixIdentity (GpMatrix *matrix, BOOL *result);
+GpStatus WINGDIPAPI GdipIsMatrixEqual (GpMatrix *matrix, GpMatrix *matrix2, BOOL *result);
 
 #endif
