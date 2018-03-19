c00cbc7252f9d7553ad11d4348ff29e3ba662396
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-03-19 21:20:05 +0100

    Easier way to check for error

--- .jenkins-linux.sh
+++ .jenkins-linux.sh
@@ -3,4 +3,4 @@
 export CFLAGS=-ggdb3
 ./autogen.sh --enable-warnaserror
 make -w
-/bin/sh -c 'if make check -k -w; then true; else cat ./tests/test-suite.log; exit 1; fi'
+make check -k -w || cat ./tests/test-suite.log; exit 1
