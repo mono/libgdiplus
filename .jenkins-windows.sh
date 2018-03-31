e9e0c5b6b1a17af7844f4ab49576cc434515885e
Hugh Bellamy <hughbellars@gmail.com>
2018-03-31 22:39:45 +0100

    Convert to vckpg

--- .jenkins-windows.sh
+++ .jenkins-windows.sh
@@ -2,16 +2,13 @@
 
 export PATH=/usr/bin:/usr/local/bin:$PATH
 
-# NOTE: update README.md too when updating these urls
-wget "https://dl.hexchat.net/gtk/gtk-win32-2018-03-13.7z" -O gtk-Win32.7z
-wget "https://dl.hexchat.net/gtk/gtk-x64-2018-03-13.7z" -O gtk-x64.7z
-wget "https://dist.nuget.org/win-x86-commandline/v4.1.0/nuget.exe" -O nuget.exe
+git clone https://github.com/Microsoft/vcpkg.git
+bootstrap-vcpkg.bat
+vcpkg integrate install
 
-7z x gtk-Win32.7z -ogtk
-7z x gtk-x64.7z -ogtk
-chmod +x nuget.exe
+vcpkg install giflib libjpeg-turbo libpng cairo glib tiff libexif glib
+vcpkg install giflib libjpeg-turbo libpng cairo glib tiff libexif glib --triplet x64-windows
 
-./nuget.exe restore
 /cygdrive/c/Program\ Files\ \(x86\)/MSBuild/14.0/Bin/MSBuild.exe /p:PlatformToolset=v140 /p:Platform=Win32 /p:Configuration=Debug libgdiplus.sln
 /cygdrive/c/Program\ Files\ \(x86\)/MSBuild/14.0/Bin/MSBuild.exe /p:PlatformToolset=v140 /p:Platform=Win32 /p:Configuration=Release libgdiplus.sln
 /cygdrive/c/Program\ Files\ \(x86\)/MSBuild/14.0/Bin/MSBuild.exe /p:PlatformToolset=v140 /p:Platform=x64 /p:Configuration=Debug libgdiplus.sln
