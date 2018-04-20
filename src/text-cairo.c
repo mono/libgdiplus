4e974dca3c0efe4124177d4c79b73946e4e0cee5
FrozenShade666 <siara.kamil@gmail.com>
2018-04-20 17:49:11 +0200

    Update text-cairo.c (#370)
    https://github.com/mono/mono/issues/8272
    
    Some characters were recognized as trailing spaces.

--- src/text-cairo.c
+++ src/text-cairo.c
@@ -237,7 +237,7 @@ MeasureString (GpGraphics *graphics, GDIPCONST WCHAR *stringUnicode, int *length
 
 	/* unless specified we don't consider the trailing spaces, unless there is just one space (#80680) */
 	if ((format->formatFlags & StringFormatFlagsMeasureTrailingSpaces) == 0) {
-		while ((StringLen > 0) && (isspace ((int) ((unsigned char) *(Src + StringLen - 1)))))
+		while ((StringLen > 0) && (isspace ((int) ( *(Src + StringLen - 1)))))
 			StringLen--;
 		if (StringLen == 0)
 			StringLen = 1;
