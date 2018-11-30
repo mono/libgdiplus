b65af950cbf5d055b2fa833fff45f3f753d1d977
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-11-30 17:32:56 +0100

    Fix cflags

--- .jenkins-osx.sh
+++ .jenkins-osx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+EXTRA_CFLAGS="-ggdb3 -O2"
+
 if [[ ${label} == *'osx-i386'* ]]; then
 	EXTRA_CFLAGS="$EXTRA_CFLAGS -m32 -arch i386 -mmacosx-version-min=10.9"
 	EXTRA_LDFLAGS="$EXTRA_LDFLAGS -m32 -arch i386"
@@ -9,7 +11,6 @@ else
 	EXTRA_LDFLAGS="$EXTRA_LDFLAGS -m64 -arch x86_64" 
 fi
 
-export CFLAGS=-ggdb3
 PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig ./autogen.sh --enable-warnaserror CFLAGS="$CFLAGS $EXTRA_CFLAGS" LDFLAGS="$LDFLAGS $EXTRA_LDFLAGS" $EXTRA_CONF_FLAGS
 make -w
 make check -k -w && result=0 || result=$?
