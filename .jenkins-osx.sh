4bc761b02dff7106989998e1961d8e294fa8aa5a
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-11-30 17:24:54 +0100

    Update configure flags for OSX in Jenkins script

--- .jenkins-osx.sh
+++ .jenkins-osx.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -xe
 
+if [[ ${label} == *'osx-i386'* ]]; then
+	EXTRA_CFLAGS="$EXTRA_CFLAGS -m32 -arch i386 -mmacosx-version-min=10.9"
+	EXTRA_LDFLAGS="$EXTRA_LDFLAGS -m32 -arch i386"
+	EXTRA_CONF_FLAGS="--host=i386-apple-darwin13.0.0 --build=i386-apple-darwin13.0.0"
+else	
+	EXTRA_CFLAGS="$EXTRA_CFLAGS -m64 -arch x86_64 -mmacosx-version-min=10.9"
+	EXTRA_LDFLAGS="$EXTRA_LDFLAGS -m64 -arch x86_64" 
+fi
+
 export CFLAGS=-ggdb3
-PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig ./autogen.sh --enable-warnaserror
+PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig ./autogen.sh --enable-warnaserror CFLAGS="$CFLAGS $EXTRA_CFLAGS" LDFLAGS="$LDFLAGS $EXTRA_LDFLAGS" $EXTRA_CONF_FLAGS
 make -w
 make check -k -w && result=0 || result=$?
 
