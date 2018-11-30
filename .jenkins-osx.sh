75cc7c0874623ea3af1f6a0e6370ade4e989e85b
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-11-30 17:42:04 +0100

    Fix cflags

--- .jenkins-osx.sh
+++ .jenkins-osx.sh
@@ -9,7 +9,6 @@ if [[ ${label} == *'osx-i386'* ]]; then
 else	
 	EXTRA_CFLAGS="$EXTRA_CFLAGS -m64 -arch x86_64 -mmacosx-version-min=10.9"
 	EXTRA_LDFLAGS="$EXTRA_LDFLAGS -m64 -arch x86_64" 
-	EXTRA_CONF_FLAGS="--host=x86_64-apple-darwin13.0.0 --build=x86_64-apple-darwin13.0.0"
 fi
 
 export CFLAGS="$CFLAGS $EXTRA_CFLAGS"
