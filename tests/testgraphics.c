mono/libgdiplus

commit 5dd3b5da1565a99912ff8570054dad26a04e9c08

Various region scans fixes

src/graphics-cairo.c
@@ -1107,13 +1107,14 @@ cairo_SetGraphicsClip (GpGraphics *graphics)
 		if (work->tree && work->tree->path)
 			gdip_plot_path (graphics, work->tree->path, FALSE);
 		else {
-			int count;
+			UINT count;
 			/* I admit that's a (not so cute) hack - anyone with a better idea ? */
 			if ((GdipGetRegionScansCount (work, &count, NULL) == Ok) && (count > 0)) {
 				GpRectF *rects = (GpRectF*) GdipAlloc (count * sizeof (GpRectF));
 				if (rects) {
-					GdipGetRegionScans (work, rects, &count, NULL);
-						for (i = 0, rect = rects; i < count; i++, rect++) {
+					INT countTemp;
+					GdipGetRegionScans (work, rects, &countTemp, NULL);
+					for (i = 0, rect = rects; i < countTemp; i++, rect++) {
 						gdip_cairo_rectangle (graphics, rect->X, rect->Y, rect->Width, rect->Height, FALSE);
 					}
 					GdipFree (rects);

src/region-bitmap.c
@@ -935,13 +935,12 @@ process_line (GpRegionBitmap *bitmap, int y, int *x, int *w)
  * gdip_region_bitmap_get_scans:
  * @bitmap: a GpRegionBitmap
  * @rect: a pointer to an array of GpRectF
- * @count: the number of GpRectF in the array
  *
  * Convert the scan lines of the bitmap into an array of GpRectF. The return
  * value represents the actual number of GpRectF entries that were generated.
  */
 int
-gdip_region_bitmap_get_scans (GpRegionBitmap *bitmap, GpRectF *rect, int count)
+gdip_region_bitmap_get_scans (GpRegionBitmap *bitmap, GpRectF *rect)
 {
 	if (!bitmap || !bitmap->Mask)
 		return 0;
@@ -975,7 +974,7 @@ gdip_region_bitmap_get_scans (GpRegionBitmap *bitmap, GpRectF *rect, int count)
 				actual.Width = w;
 				actual.Height = 1;
 
-				if (rect && (n < count)) {
+				if (rect) {
 					rect [n].X = actual.X;
 					rect [n].Y = actual.Y;
 					rect [n].Width = actual.Width;

src/region-bitmap.h
@@ -63,7 +63,7 @@ BOOL gdip_region_bitmap_compare (GpRegionBitmap *shape1, GpRegionBitmap *shape2)
 BOOL gdip_region_bitmap_is_point_visible (GpRegionBitmap *bitmap, int x, int y) GDIP_INTERNAL;
 BOOL gdip_region_bitmap_is_rect_visible (GpRegionBitmap *bitmap, GpRect *rect) GDIP_INTERNAL;
 
-int gdip_region_bitmap_get_scans (GpRegionBitmap *bitmap, GpRectF *rect, int count) GDIP_INTERNAL;
+int gdip_region_bitmap_get_scans (GpRegionBitmap *bitmap, GpRectF *rect) GDIP_INTERNAL;
 
 void gdip_region_bitmap_get_smallest_rect (GpRegionBitmap *bitmap, GpRect *rect) GDIP_INTERNAL;
 void gdip_region_bitmap_shrink (GpRegionBitmap *bitmap, BOOL always_shrink) GDIP_INTERNAL;

src/region.c
@@ -1638,12 +1638,12 @@ GdipIsVisibleRegionRectI (GpRegion *region, int x, int y, int width, int height,
 
 
 GpStatus WINGDIPAPI
-GdipGetRegionScansCount (GpRegion *region, int* count, GpMatrix* matrix)
+GdipGetRegionScansCount (GpRegion *region, UINT *count, GpMatrix *matrix)
 {
 	GpRegion *work = NULL;
 	GpStatus status;
 
-	if (!region || !count)
+	if (!region || !matrix || !count)
 		return InvalidParameter;
 
 	/* apply any user supplied matrix transformation */
@@ -1687,7 +1687,7 @@ GdipGetRegionScansCount (GpRegion *region, int* count, GpMatrix* matrix)
 	case RegionTypePath:
 		/* ensure the bitmap is usable */
 		gdip_region_bitmap_ensure (work);
-		*count = gdip_region_bitmap_get_scans (work->bitmap, NULL, -1);
+		*count = gdip_region_bitmap_get_scans (work->bitmap, NULL);
 		break;
 	default:
 		g_warning ("unknown type 0x%08X", region->type);
@@ -1706,7 +1706,7 @@ GdipGetRegionScans (GpRegion *region, GpRectF* rects, int* count, GpMatrix* matr
 	GpRegion *work = NULL;
 	GpStatus status;
 
-	if (!region || !rects|| !count)
+	if (!region || !count || !matrix)
 		return InvalidParameter;
 
 	/* apply any user supplied matrix transformation */
@@ -1745,13 +1745,14 @@ GdipGetRegionScans (GpRegion *region, GpRectF* rects, int* count, GpMatrix* matr
 
 	switch (region->type) {
 	case RegionTypeRect:
-		memcpy (rects, work->rects, sizeof (GpRectF) * *count);
+		if (rects)
+			memcpy (rects, work->rects, sizeof (GpRectF) * work->cnt);
 		*count = work->cnt;
 		break;
 	case RegionTypePath:
 		/* ensure the bitmap is usable */
 		gdip_region_bitmap_ensure (work);
-		*count = gdip_region_bitmap_get_scans (work->bitmap, rects, *count);
+		*count = gdip_region_bitmap_get_scans (work->bitmap, rects);
 		break;
 	default:
 		g_warning ("unknown type 0x%08X", region->type);

src/region.h
@@ -49,7 +49,7 @@ GpStatus WINGDIPAPI GdipIsVisibleRegionPointI(GpRegion *region, int x, int y, Gp
 GpStatus WINGDIPAPI GdipIsVisibleRegionRect(GpRegion *region, float x, float y, float width, float height, GpGraphics *graphics, BOOL *result);
 GpStatus WINGDIPAPI GdipIsVisibleRegionRectI(GpRegion *region, int x, int y, int width, int height, GpGraphics *graphics, BOOL *result);
 
-GpStatus WINGDIPAPI GdipGetRegionScansCount(GpRegion *region, int* count, GpMatrix* matrix);
+GpStatus WINGDIPAPI GdipGetRegionScansCount(GpRegion *region, UINT *count, GpMatrix *matrix);
 GpStatus WINGDIPAPI GdipGetRegionScans(GpRegion *region, GpRectF* rects, int* count, GpMatrix* matrix);
 
 GpStatus WINGDIPAPI GdipIsEqualRegion(GpRegion *region, GpRegion *region2, GpGraphics *graphics, BOOL *result);

tests/testregion.c
@@ -267,6 +267,273 @@ static void test_createRegionPath ()
 	GdipDeletePath (path);
 }
 
+static void test_getRegionScansCount ()
+{
+	GpStatus status;
+	GpPath *path;
+	GpRegion *region;
+	GpMatrix *matrix;
+	UINT count;
+
+	GdipCreateMatrix (&matrix);
+
+	// Rect region.
+	GpRectF rect = {10, 20, 30, 40};
+	GdipCreateRegionRect (&rect, &region);
+	
+	status = GdipGetRegionScansCount (region, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+	GdipDeleteRegion (region);
+
+	// Path region.
+	GdipCreatePath (FillModeWinding, &path);
+	GdipAddPathRectangle (path, 10, 20, 30, 40);
+	GdipCreateRegionPath (path, &region);
+	
+	status = GdipGetRegionScansCount (region, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+	GdipDeleteRegion (region);
+	
+	// Empty region.
+	GdipCreateRegion (&region);
+	GdipSetEmpty (region);
+	
+	status = GdipGetRegionScansCount (region, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 0);
+	GdipDeleteRegion (region);
+
+	// Infinite region.
+	GdipCreateRegion (&region);
+	
+	status = GdipGetRegionScansCount (region, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+
+	// Negative tests.
+	status = GdipGetRegionScansCount (NULL, &count, matrix);
+	assertEqualInt (status, InvalidParameter);
+
+	status = GdipGetRegionScansCount (region, NULL, matrix);
+	assertEqualInt (status, InvalidParameter);
+
+	status = GdipGetRegionScansCount (region, &count, NULL);
+	assertEqualInt (status, InvalidParameter);
+
+	GdipDeleteMatrix (matrix);
+	GdipDeleteRegion (region);
+}
+
+static void test_getRegionScans ()
+{
+	GpStatus status;
+	GpPath *path;
+	GpRegion *region;
+	GpMatrix *matrix;
+	GpRectF scans[1];
+	INT count;
+
+	GdipCreateMatrix (&matrix);
+
+	// Rect region - non null rects.
+	GpRectF rect = {10, 20, 30, 40};
+	GdipCreateRegionRect (&rect, &region);
+	
+	count = 0xFF;
+	status = GdipGetRegionScans (region, scans, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualFloat (scans[0].X, 10);
+	assertEqualFloat (scans[0].Y, 20);
+	assertEqualFloat (scans[0].Width, 30);
+	assertEqualFloat (scans[0].Height, 40);
+	assertEqualInt (count, 1);
+
+	// Rect region - null rects.
+	count = 0xFF;
+	status = GdipGetRegionScans (region, NULL, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+	GdipDeleteRegion (region);
+
+	// Path region - non null rects.
+	GdipCreatePath (FillModeWinding, &path);
+	GdipAddPathRectangle (path, 10, 20, 30, 40);
+	GdipCreateRegionPath (path, &region);
+	
+	count = 0xFF;
+	status = GdipGetRegionScans (region, scans, &count, matrix);
+	assertEqualInt (status, Ok);
+	// FIXME: these are incorrect.
+#if defined(USE_WINDOWS_GDIPLUS)
+	assertEqualFloat (scans[0].X, 10);
+	assertEqualFloat (scans[0].Y, 20);
+	assertEqualFloat (scans[0].Width, 30);
+	assertEqualFloat (scans[0].Height, 40);
+#endif
+	assertEqualInt (count, 1);
+
+	// Path region - null rects.
+	count = 0xFF;
+	status = GdipGetRegionScans (region, NULL, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+	GdipDeleteRegion (region);
+	
+	// Empty region - non null rects.
+	GdipCreateRegion (&region);
+	GdipSetEmpty (region);
+	
+	count = 0xFF;
+	status = GdipGetRegionScans (region, scans, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 0);
+
+	// Empty region - null rects.
+	count = 0xFF;
+	status = GdipGetRegionScans (region, NULL, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 0);
+	GdipDeleteRegion (region);
+
+	// Infinite region - non null rects.
+	GdipCreateRegion (&region);
+	
+	count = 0xFF;
+	status = GdipGetRegionScans (region, scans, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualFloat (scans[0].X, -4194304);
+	assertEqualFloat (scans[0].Y, -4194304);
+	assertEqualFloat (scans[0].Width, 8388608);
+	assertEqualFloat (scans[0].Height, 8388608);
+	assertEqualInt (count, 1);
+
+	// Infinite region - null rects.
+	count = 0xFF;
+	status = GdipGetRegionScans (region, NULL, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+
+	// Negative tests.
+	status = GdipGetRegionScans (NULL, scans, &count, matrix);
+	assertEqualInt (status, InvalidParameter);
+
+	status = GdipGetRegionScans (region, scans, NULL, matrix);
+	assertEqualInt (status, InvalidParameter);
+
+	status = GdipGetRegionScans (region, scans, &count, NULL);
+	assertEqualInt (status, InvalidParameter);
+
+	GdipDeleteMatrix (matrix);
+	GdipDeleteRegion (region);
+}
+
+#if defined(USE_WINDOWS_GDIPLUS)
+static void test_getRegionScansI ()
+{
+	GpStatus status;
+	GpPath *path;
+	GpRegion *region;
+	GpMatrix *matrix;
+	GpRect scans[1];
+	INT count;
+
+	GdipCreateMatrix (&matrix);
+
+	// Rect region - non null rects.
+	GpRectF rect = {10, 20, 30, 40};
+	GdipCreateRegionRect (&rect, &region);
+	
+	count = 0xFF;
+	status = GdipGetRegionScansI (region, scans, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualFloat (scans[0].X, 10);
+	assertEqualFloat (scans[0].Y, 20);
+	assertEqualFloat (scans[0].Width, 30);
+	assertEqualFloat (scans[0].Height, 40);
+	assertEqualInt (count, 1);
+
+	// Rect region - null rects.
+	count = 0xFF;
+	status = GdipGetRegionScansI (region, NULL, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+	GdipDeleteRegion (region);
+
+	// Path region - non null rects.
+	GdipCreatePath (FillModeWinding, &path);
+	GdipAddPathRectangle (path, 10, 20, 30, 40);
+	GdipCreateRegionPath (path, &region);
+	
+	count = 0xFF;
+	status = GdipGetRegionScansI (region, scans, &count, matrix);
+	assertEqualInt (status, Ok);
+	// FIXME: these are incorrect.
+#if defined(USE_WINDOWS_GDIPLUS)
+	assertEqualFloat (scans[0].X, 10);
+	assertEqualFloat (scans[0].Y, 20);
+	assertEqualFloat (scans[0].Width, 30);
+	assertEqualFloat (scans[0].Height, 40);
+#endif
+	assertEqualInt (count, 1);
+
+	// Path region - null rects.
+	count = 0xFF;
+	status = GdipGetRegionScansI (region, NULL, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+	GdipDeleteRegion (region);
+	
+	// Empty region - non null rects.
+	GdipCreateRegion (&region);
+	GdipSetEmpty (region);
+	
+	count = 0xFF;
+	status = GdipGetRegionScansI (region, scans, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 0);
+
+	// Empty region - null rects.
+	count = 0xFF;
+	status = GdipGetRegionScansI (region, NULL, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 0);
+	GdipDeleteRegion (region);
+
+	// Infinite region - non null rects.
+	GdipCreateRegion (&region);
+	
+	count = 0xFF;
+	status = GdipGetRegionScansI (region, scans, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualFloat (scans[0].X, -4194304);
+	assertEqualFloat (scans[0].Y, -4194304);
+	assertEqualFloat (scans[0].Width, 8388608);
+	assertEqualFloat (scans[0].Height, 8388608);
+	assertEqualInt (count, 1);
+
+	// Infinite region - null rects.
+	count = 0xFF;
+	status = GdipGetRegionScansI (region, NULL, &count, matrix);
+	assertEqualInt (status, Ok);
+	assertEqualInt (count, 1);
+
+	// Negative tests.
+	status = GdipGetRegionScansI (NULL, scans, &count, matrix);
+	assertEqualInt (status, InvalidParameter);
+
+	status = GdipGetRegionScansI (region, scans, NULL, matrix);
+	assertEqualInt (status, InvalidParameter);
+
+	status = GdipGetRegionScansI (region, scans, &count, NULL);
+	assertEqualInt (status, InvalidParameter);
+
+	GdipDeleteMatrix (matrix);
+	GdipDeleteRegion (region);
+}
+#endif
+
 int
 main (int argc, char**argv)
 {
@@ -279,6 +546,12 @@ main (int argc, char**argv)
 	test_createRegionRect ();
 	test_createRegionRectI ();
 	test_createRegionPath ();
+	test_getRegionScansCount ();
+	test_getRegionScans ();
+	// FIXME: implement GdipGetRegionScansI.
+#if defined(USE_WINDOWS_GDIPLUS)
+	test_getRegionScansI ();
+#endif
 
 	GdipDisposeImage (image);
 	GdipDeleteGraphics (graphics);

commit 1586f1a03cff7d45039b6611e0923d61f134fac1

Respect page units when current transformation matrix is passed to cairo (#551)
* Add failed tests for page unit and world transform dependency (#545)

* test_world_transform_respects_page_unit_document - tests
GdipSetWorldTransform and UnitDocument
* test_world_transform_respects_page_unit_point - tests
GdipTranslateWorldTransform and UnitPoint

* Respect page units when passing transformation matrix to cairo (#545)

* general.c - implement gdip_cairo_set_matrix function that converts
translation part of a matrix from page units to cairo coordinates
* graphics-cairo.c - fix typo when input parameter 'matrix' was not used
* customlinecap.c, graphics-cairo.c, graphics.c, image.c, pen.c - use
gdip_cairo_set_matrix instead of cairo_set_matrix when input matrix is
expressed in page units

* Fix clipping of non-rectangular paths (#547, #552)

Clipping of paths and path-based regions was broken in
5dd3b5da1565a99912ff8570054dad26a04e9c08. This commit fixes the issue.

* graphics-cairo.c - do not pass NULL matrix to GdipGetRegionScansCount
and GdipGetRegionScans

* testclip.c - add test for clipping of paths and regions based on paths

* Fix indent in test

Fixes #545.

src/customlinecap.c
@@ -253,7 +253,7 @@ gdip_custom_linecap_draw (GpGraphics *graphics, GpPen *pen, GpCustomLineCap *cus
 
 		gdip_pen_setup (graphics, pen);
 		cairo_stroke (graphics->ct);
-		cairo_set_matrix (graphics->ct, graphics->copy_of_ctm);
+		gdip_cairo_set_matrix (graphics, graphics->copy_of_ctm);
 	}
 
 	/* FIXME: handle fill_path */

src/general.c
@@ -615,6 +615,32 @@ gdip_cairo_curve_to (GpGraphics *graphics, double x1, double y1, double x2, doub
 	cairo_curve_to (graphics->ct, x1, y1, x2, y2, x3, y3);
 }
 
+void
+gdip_cairo_set_matrix (GpGraphics *graphics, GpMatrix *matrixPageUnits)
+{
+	float x0 = matrixPageUnits->x0;
+	float y0 = matrixPageUnits->y0;
+
+	/* avoid unit conversion whenever possible */
+	if (!OPTIMIZE_CONVERSION (graphics)) {
+		x0 = gdip_unitx_convgr (graphics, x0);
+		y0 = gdip_unity_convgr (graphics, y0);
+	}
+
+	/* do not apply antialiasing trick to transformation matrix */
+
+	/* put everything between cairo limits */
+	x0 = CAIRO_LIMIT (x0);
+	y0 = CAIRO_LIMIT (y0);
+
+	GpMatrix matrixCopy;
+	gdip_cairo_matrix_copy (&matrixCopy, matrixPageUnits);
+	matrixCopy.x0 = x0;
+	matrixCopy.y0 = y0;
+
+	cairo_set_matrix (graphics->ct, &matrixCopy);
+}
+
 void gdip_RectF_from_Rect (const GpRect* rect, GpRectF* rectf)
 {
 	rectf->X = rect->X;

src/graphics-cairo.c
@@ -54,7 +54,7 @@ fill_graphics_with_brush (GpGraphics *graphics, GpBrush *brush, BOOL stroke)
 	/* Set the matrix back to graphics->copy_of_ctm for other functions.
 	 * This overwrites the matrix set by brush setup.
 	 */
-	cairo_set_matrix (graphics->ct, graphics->copy_of_ctm);
+	gdip_cairo_set_matrix (graphics, graphics->copy_of_ctm);
 
 	return gdip_get_status (cairo_status (graphics->ct));
 }
@@ -69,7 +69,7 @@ stroke_graphics_with_pen (GpGraphics *graphics, GpPen *pen)
 	/* Set the matrix back to graphics->copy_of_ctm for other functions.
 	 * This overwrites the matrix set by pen setup.
 	 */
-	cairo_set_matrix (graphics->ct, graphics->copy_of_ctm);
+	gdip_cairo_set_matrix (graphics, graphics->copy_of_ctm);
 
 	return gdip_get_status (cairo_status (graphics->ct));
 }
@@ -853,12 +853,14 @@ cairo_SetGraphicsClip (GpGraphics *graphics)
 			gdip_plot_path (graphics, work->tree->path, FALSE);
 		else {
 			UINT count;
+			GpMatrix matrix;
+			cairo_matrix_init_identity (&matrix);
 			/* I admit that's a (not so cute) hack - anyone with a better idea ? */
-			if ((GdipGetRegionScansCount (work, &count, NULL) == Ok) && (count > 0)) {
+			if ((GdipGetRegionScansCount (work, &count, &matrix) == Ok) && (count > 0)) {
 				GpRectF *rects = (GpRectF*) GdipAlloc (count * sizeof (GpRectF));
 				if (rects) {
 					INT countTemp;
-					GdipGetRegionScans (work, rects, &countTemp, NULL);
+					GdipGetRegionScans (work, rects, &countTemp, &matrix);
 					for (i = 0, rect = rects; i < countTemp; i++, rect++) {
 						gdip_cairo_rectangle (graphics, rect->X, rect->Y, rect->Width, rect->Height, FALSE);
 					}
@@ -895,7 +897,7 @@ cairo_ResetClip (GpGraphics *graphics)
 GpStatus
 cairo_ResetWorldTransform (GpGraphics *graphics)
 {
-	cairo_set_matrix (graphics->ct, graphics->copy_of_ctm);
+	gdip_cairo_set_matrix (graphics, graphics->copy_of_ctm);
 	cairo_reset_clip (graphics->ct);
 	cairo_SetGraphicsClip (graphics);
 	return gdip_get_status (cairo_status (graphics->ct));
@@ -904,7 +906,7 @@ cairo_ResetWorldTransform (GpGraphics *graphics)
 GpStatus
 cairo_SetWorldTransform (GpGraphics *graphics, GpMatrix *matrix)
 {
-	cairo_set_matrix (graphics->ct, graphics->copy_of_ctm);
+	gdip_cairo_set_matrix (graphics, matrix);
 	cairo_SetGraphicsClip (graphics);
 	return gdip_get_status (cairo_status (graphics->ct));
 }

src/graphics-private.h
@@ -169,6 +169,8 @@ void gdip_cairo_line_to (GpGraphics *graphics, double x, double y, BOOL convert_
 void gdip_cairo_curve_to (GpGraphics *graphics, double x1, double y1, double x2, double y2, double x3, double y3, 
 	BOOL convert_units, BOOL antialiasing) GDIP_INTERNAL;
 
+void gdip_cairo_set_matrix (GpGraphics *graphics, GpMatrix *matrixPageUnits) GDIP_INTERNAL;
+
 #ifdef CAIRO_HAS_QUARTZ_SURFACE
 
 #if __i386__

src/graphics.c
@@ -529,7 +529,7 @@ GdipRestoreGraphics (GpGraphics *graphics, GraphicsState state)
 	graphics->saved_status_pos = state;
 
 	/* re-adjust clipping (region and matrix) */
-	cairo_set_matrix (graphics->ct, graphics->copy_of_ctm);
+	gdip_cairo_set_matrix (graphics, graphics->copy_of_ctm);
 
 	/* GdipCloneRegion was called, but for some reason, not registred as an allocation */
 	/* coverity[freed_arg] */

src/image.c
@@ -544,7 +544,7 @@ GdipDrawImagePoints (GpGraphics *graphics, GpImage *image, GDIPCONST GpPointF *d
 		metacontext = gdip_metafile_play_setup ((GpMetafile*)image, graphics, tRect.X, tRect.Y, tRect.Width, tRect.Height);
 
 		cairo_get_matrix (graphics->ct, &orig_matrix);
-		cairo_set_matrix (graphics->ct, matrix);
+		gdip_cairo_set_matrix (graphics, matrix);
 		status = gdip_metafile_play (metacontext);
 		GdipDeleteMatrix (matrix);
 
@@ -575,7 +575,7 @@ GdipDrawImagePoints (GpGraphics *graphics, GpImage *image, GDIPCONST GpPointF *d
 	cairo_pattern_reference(org_pattern);
 	
 	cairo_get_matrix(graphics->ct, &orig_matrix);
-	cairo_set_matrix (graphics->ct, matrix);
+	gdip_cairo_set_matrix (graphics, matrix);
 	cairo_set_source_surface (graphics->ct, image->surface, 0, 0);
 	
 	cairo_paint (graphics->ct);	
@@ -1056,7 +1056,7 @@ GdipDrawImagePointsRect (GpGraphics *graphics, GpImage *image, GDIPCONST GpPoint
 	}
 
 	cairo_get_matrix (graphics->ct, &orig_matrix);
-	cairo_set_matrix (graphics->ct, matrix);
+	gdip_cairo_set_matrix (graphics, matrix);
 	status = GdipDrawImageRectRect (graphics, image, rect.X, rect.Y, rect.Width, rect.Height, srcx, srcy, 
 		srcwidth, srcheight, srcUnit, imageAttributes, callback, callbackData);
 	cairo_set_matrix (graphics->ct, &orig_matrix);

src/pen.c
@@ -168,7 +168,7 @@ gdip_pen_setup (GpGraphics *graphics, GpPen *pen)
 		/* *both* X and Y are affected if either is 0 */
 		product.xx = product.yy = 0.0001f;
 	}
-	cairo_set_matrix (graphics->ct, &product);
+	gdip_cairo_set_matrix (graphics, &product);
 
 	/* Don't need to setup, if pen is the same as the cached pen and
 	 * it is not changed. Just comparing pointers may not be sufficient

tests/testclip.c
@@ -193,6 +193,68 @@ test_gdip_clip_transform()
 	GdipFree (scan0);
 }
 
+static void
+test_gdip_clip_path ()
+{
+	GpBitmap *bitmap = 0;
+	GpGraphics *graphics;
+
+	C (GdipCreateBitmapFromScan0 (612, 792, 0, PixelFormat32bppARGB, NULL, &bitmap));
+	C (GdipBitmapSetResolution (bitmap, 72, 72));
+	C (GdipGetImageGraphicsContext (bitmap, &graphics));
+
+	C (GdipSetClipRect (graphics, 0, 0, 612, 792, CombineModeIntersect));
+
+	GpRectF rc = { 100, 100, 50, 50 };
+	GpPath *path;
+	C (GdipCreatePath (FillModeAlternate, &path));
+	{
+		C (GdipAddPathRectangle (path, rc.X, rc.Y, rc.Width, rc.Height));
+
+		// This rectangle should be overlapped by the black rectangle
+		GpSolidFill *pathBrush;
+		C (GdipCreateSolidFill (0xFF00FF00, &pathBrush));
+		C (GdipFillPath (graphics, pathBrush, path));
+		C (GdipDeleteBrush (pathBrush));
+
+		// Test clipping by GdipSetClipPath (issue #552)
+		C (GdipSetClipPath (graphics, path, CombineModeIntersect));
+
+		// Also test clipping by GdipSetClipRegion (issue #547)
+		GpRegion *rgn;
+		C (GdipCreateRegionPath (path, &rgn));
+		C (GdipSetClipRegion (graphics, rgn, CombineModeIntersect));
+		C (GdipDeleteRegion (rgn));
+	}
+	C (GdipDeletePath (path));
+
+	const ARGB expectedColor = 0xFF000000;
+	GpSolidFill *brush;
+	C (GdipCreateSolidFill (expectedColor, &brush));
+	C (GdipFillRectangleI (graphics, brush, rc.X, rc.Y, rc.Width, rc.Height));
+	C (GdipDeleteBrush (brush));
+
+	//CLSID png_clsid = { 0x557cf406, 0x1a04, 0x11d3, { 0x9a, 0x73, 0x0, 0x0, 0xf8, 0x1e, 0xf3, 0x2e } };
+	//WCHAR *filePath = createWchar ("test_gdip_clip_path.png");
+	//C (GdipSaveImageToFile (bitmap, filePath, &png_clsid, NULL));
+
+	GpPoint points[] = {
+		{ rc.X, rc.Y },
+		{ rc.X + rc.Width / 2, rc.Y + rc.Height / 2 },
+		{ rc.X + rc.Width - 1, rc.Y + rc.Height - 1 }
+	};
+	for (int i = 0; i < sizeof (points) / sizeof (points[0]); i += 2) {
+		GpPoint *pt = &points[i];
+
+		ARGB color;
+		C (GdipBitmapGetPixel (bitmap, pt->X, pt->Y, &color));
+		assertEqualInt (color, expectedColor);
+	}
+
+	C (GdipDeleteGraphics (graphics));
+	C (GdipDisposeImage (bitmap));
+}
+
 int
 main(int argc, char**argv)
 {
@@ -200,6 +262,7 @@ main(int argc, char**argv)
 
 	test_gdip_clip();
 	test_gdip_clip_transform();
+	test_gdip_clip_path ();
 
 	SHUTDOWN;
 	return 0;

tests/testgraphics.c
@@ -3498,6 +3498,130 @@ static void test_world_transform_in_container ()
 	GdipDeleteGraphics (graphics);
 }
 
+static void test_world_transform_respects_page_unit_document ()
+{
+	GpStatus status;
+	GpBitmap *bitmap;
+	GpGraphics *graphics;
+	GpMatrix *matrix;
+	GpSolidFill *brush;
+
+	// Test Graphics with UnitDocument and 72 dpi
+	status = GdipCreateBitmapFromScan0 (612, 792, 0, PixelFormat32bppARGB, NULL, &bitmap);
+	assertEqualInt (status, Ok);
+
+	status = GdipBitmapSetResolution (bitmap, 72, 72);
+	assertEqualInt (status, Ok);
+
+	status = GdipGetImageGraphicsContext (bitmap, &graphics);
+	assertEqualInt (status, Ok);
+
+	status = GdipSetPageUnit (graphics, UnitDocument);
+	assertEqualInt (status, Ok);
+
+	const ARGB FillColor = 0xFF000000;
+	status = GdipCreateSolidFill (FillColor, &brush);
+	assertEqualInt (status, Ok);
+
+	int rectX = 360;
+	int rectY = 111;
+	GdipCreateMatrix2 (1, 0, 0, 1, rectX, rectY, &matrix);
+	status = GdipSetWorldTransform (graphics, matrix);
+	assertEqualInt (status, Ok);
+
+	int rectWidth = 74;
+	int rectHeight = 72;
+	status = GdipFillRectangleI (graphics, brush, 0, 0, rectWidth, rectHeight);
+	assertEqualInt (status, Ok);
+
+	//CLSID png_clsid = { 0x557cf406, 0x1a04, 0x11d3, { 0x9a, 0x73, 0x0, 0x0, 0xf8, 0x1e, 0xf3, 0x2e } };
+	//WCHAR *filePath = createWchar ("test_world_transform_respects_page_unit_document.png");
+	//status = GdipSaveImageToFile (bitmap, filePath, &png_clsid, NULL);
+
+	int points[] = {
+		rectX + 2,
+		rectY + 2,
+		rectX + rectWidth / 2,
+		rectY + rectHeight / 2,
+		rectX + rectWidth - 1,
+		rectY + rectHeight - 1
+	};
+	for (int i = 0; i < sizeof (points) / sizeof (points[0]); i += 2) {
+		ARGB color;
+		status = GdipBitmapGetPixel (bitmap, (int)(points[i] * 72 / 300.0f), (int)(points[i + 1] * 72 / 300.0f), &color);
+		assertEqualInt (status, Ok);
+		assertEqualInt (color, FillColor);
+	}
+
+	GdipDisposeImage ((GpImage *)bitmap);
+	GdipDeleteGraphics (graphics);
+	GdipDeleteBrush (brush);
+}
+
+static void test_world_transform_respects_page_unit_point ()
+{
+	GpStatus status;
+	GpBitmap *bitmap;
+	GpGraphics *graphics;
+	GpSolidFill *brush;
+	const float TargetResolution = 300;
+	double scaleFactor = TargetResolution / 72;
+
+	// Test Graphics with UnitPoint and 72 dpi
+	status = GdipCreateBitmapFromScan0 ((int)(612 * scaleFactor), (int)(792 * scaleFactor), 0, PixelFormat32bppARGB, NULL, &bitmap);
+	assertEqualInt (status, Ok);
+
+	status = GdipBitmapSetResolution (bitmap, TargetResolution, TargetResolution);
+	assertEqualInt (status, Ok);
+
+	status = GdipGetImageGraphicsContext (bitmap, &graphics);
+	assertEqualInt (status, Ok);
+
+	status = GdipSetPageUnit (graphics, UnitPoint);
+	assertEqualInt (status, Ok);
+
+	const ARGB FillColor = 0xFF000000;
+	status = GdipCreateSolidFill (FillColor, &brush);
+	assertEqualInt (status, Ok);
+
+	int rectX = 360;
+	int rectY = 111;
+	int rectWidth = 74;
+	int rectHeight = 72;
+	status = GdipTranslateWorldTransform (graphics, rectX, rectY, MatrixOrderPrepend);
+	assertEqualInt (status, Ok);
+
+	status = GdipFillRectangleI (graphics, brush, 0, 0, rectWidth, rectHeight);
+	assertEqualInt (status, Ok);
+
+	//CLSID png_clsid = { 0x557cf406, 0x1a04, 0x11d3, { 0x9a, 0x73, 0x0, 0x0, 0xf8, 0x1e, 0xf3, 0x2e } };
+	//WCHAR *filePath = createWchar ("test_world_transform_respects_page_unit_point.png");
+	//status = GdipSaveImageToFile (bitmap, filePath, &png_clsid, NULL);
+
+	int points[] = {
+		rectX,
+		rectY,
+		rectX + rectWidth / 2,
+		rectY + rectHeight / 2,
+		rectX + rectWidth - 1,
+		rectY + rectHeight - 1
+	};
+	for (int i = 0; i < sizeof (points) / sizeof (points[0]); i += 2) {
+		ARGB color;
+		status = GdipBitmapGetPixel (bitmap, points[i], points[i + 1], &color);
+		assertEqualInt (status, Ok);
+		assertEqualInt (color, 0);
+
+		status = GdipBitmapGetPixel (bitmap, (int)(points[i] * scaleFactor) + 1, (int)(points[i + 1] * scaleFactor) + 1, &color);
+		assertEqualInt (status, Ok);
+		assertEqualInt (color, FillColor);
+	}
+
+	GdipDisposeImage ((GpImage *)bitmap);
+	GdipDeleteGraphics (graphics);
+	GdipDeleteBrush (brush);
+}
+
 int
 main (int argc, char**argv)
 {
@@ -3562,6 +3686,8 @@ main (int argc, char**argv)
 	test_region_mask ();
 	test_premultiplication ();
 	test_world_transform_in_container ();
+	test_world_transform_respects_page_unit_document ();
+	test_world_transform_respects_page_unit_point ();
 
 #if defined(USE_WINDOWS_GDIPLUS)
 	DestroyWindow (hwnd);