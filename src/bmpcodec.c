mono/libgdiplus

commit ac7547aa763b30f883be3eb49ea7755c8b8d3a31

Fixing Resource Leaks identified by Coverity Scan (#613)
See https://scan.coverity.com/projects/libgdiplus

(cherry picked from commit 93dc39c81dc56be2033016d1f88f1621bfaf3bf7)

src/bmpcodec.c
@@ -1203,6 +1203,7 @@ gdip_read_bmp_image (void *pointer, GpImage **image, ImageSource source)
 			status = gdip_read_bmp_indexed (pointer, pixels, upsidedown, originalFormat, originalStride, result->active_bitmap->width, result->active_bitmap->height, source);
 			if (status != Ok) {
 				gdip_bitmap_dispose (result);
+				GdipFree(pixels);
 				return status;
 			}
 		}
@@ -1210,6 +1211,7 @@ gdip_read_bmp_image (void *pointer, GpImage **image, ImageSource source)
 		status = gdip_read_bmp_scans (pointer, pixels, upsidedown, originalFormat, originalStride, result->active_bitmap->stride, result->active_bitmap->width, result->active_bitmap->height, source);
 		if (status != Ok) {
 			gdip_bitmap_dispose (result);
+			GdipFree(pixels);
 			return status;
 		}
 	}

src/bitmap.c
@@ -779,6 +779,7 @@ GdipCreateBitmapFromFile (GDIPCONST WCHAR* filename, GpBitmap **bitmap)
 		GdipDisposeImage (image);
 		return status;
 	default:
+		GdipDisposeImage (image);
 		return GenericError;
 	}
 }

src/graphics-path.c
@@ -950,8 +950,10 @@ GdipAddPathCurve2 (GpPath *path, const GpPointF *points, int count, float tensio
 	if (!tangents)
 		return OutOfMemory;
 
-	if (!gdip_path_ensure_size (path, path->count + (3 * (count - 1)) + 1))
+	if (!gdip_path_ensure_size(path, path->count + (3 * (count - 1)) + 1)) {
+		GdipFree(tangents);
 		return OutOfMemory;
+	}
 
 	append_curve (path, points, tangents, 0, count - 1, CURVE_OPEN);
 
@@ -980,9 +982,11 @@ GdipAddPathCurve3 (GpPath *path, const GpPointF *points, int count,
 	if (!tangents)
 		return OutOfMemory;
 
-	if (!gdip_path_ensure_size (path, path->count + (3 * numberOfSegments) + 1))
+	if (!gdip_path_ensure_size (path, path->count + (3 * numberOfSegments) + 1)) {
+		GdipFree(tangents);
 		return OutOfMemory;
-
+	}
+	
 	append_curve (path, points, tangents, offset, numberOfSegments, CURVE_OPEN);
 
 	GdipFree (tangents);
@@ -1008,8 +1012,10 @@ GdipAddPathClosedCurve2 (GpPath *path, const GpPointF *points, int count, float 
 	if (!tangents)
 		return OutOfMemory;
 
-	if (!gdip_path_ensure_size (path, path->count + (3 * count) + 1))
+	if (!gdip_path_ensure_size (path, path->count + (3 * count) + 1)) {
+		GdipFree(tangents);
 		return OutOfMemory;
+	}
 
 	append_curve (path, points, tangents, 0, count - 1, CURVE_CLOSE);
 

src/image.c
@@ -530,8 +530,10 @@ GdipDrawImagePoints (GpGraphics *graphics, GpImage *image, GDIPCONST GpPointF *d
 	}
 
 	/* Create a surface for this bitmap if one doesn't exist */
-	if (gdip_bitmap_ensure_surface (image) == NULL)
+	if (gdip_bitmap_ensure_surface (image) == NULL) {
+		GdipDeleteMatrix(matrix);
 		return OutOfMemory;
+	}
 
 	pattern = cairo_pattern_create_for_surface (image->surface);
 	cairo_pattern_set_filter (pattern, gdip_get_cairo_filter (graphics->interpolation));
@@ -699,8 +701,10 @@ GdipDrawImageRectRect (GpGraphics *graphics, GpImage *image,
 			/* We're ok just cloning the bitmap, we don't need the image data
 			 * and we destroy it before we leave this function */
 			status = gdip_bitmap_clone (image, &imgflipX);
-			if (status != Ok)
+			if (status != Ok) {
+				gdip_bitmap_dispose(imgflipX);
 				return status;
+			}
 
 			status = gdip_flip_x (imgflipX);
 			if (status != Ok) {
@@ -713,11 +717,15 @@ GdipDrawImageRectRect (GpGraphics *graphics, GpImage *image,
 		
 		if (flipYOn) {			
 			status = gdip_bitmap_clone (image, &imgflipY);
-			if (status != Ok)
+			if (status != Ok) {
+				gdip_bitmap_dispose(imgflipX);
+				gdip_bitmap_dispose(imgflipY);
 				return status;
+			}
 
 			status = gdip_flip_y (imgflipY);
 			if (status != Ok) {
+				gdip_bitmap_dispose (imgflipX);
 				gdip_bitmap_dispose (imgflipY);
 				return status;
 			}
@@ -727,19 +735,27 @@ GdipDrawImageRectRect (GpGraphics *graphics, GpImage *image,
 		
 		if (flipXOn && flipYOn) {			
 			status = gdip_bitmap_clone (image, &imgflipXY);
-			if (status != Ok)
+			if (status != Ok) {
+				gdip_bitmap_dispose(imgflipX);
+				gdip_bitmap_dispose(imgflipY);
+				gdip_bitmap_dispose(imgflipXY);
 				return status;
+			}
 
 			status = gdip_flip_x (imgflipXY);
 			if (status != Ok) {
-				gdip_bitmap_dispose (imgflipXY);
+				gdip_bitmap_dispose(imgflipX);
+				gdip_bitmap_dispose(imgflipY);
+				gdip_bitmap_dispose(imgflipXY);
 				return status;
 			}
 
 			status = gdip_flip_y (imgflipXY);
 			if (status != Ok) {
-				gdip_bitmap_dispose (imgflipXY);
-				return Ok;
+				gdip_bitmap_dispose(imgflipX);
+				gdip_bitmap_dispose(imgflipY);
+				gdip_bitmap_dispose(imgflipXY);
+				return status;
 			}
 
 			gdip_bitmap_ensure_surface (imgflipXY);			
@@ -940,8 +956,10 @@ GdipDrawImagePointsRectI (GpGraphics *graphics, GpImage *image, GDIPCONST GpPoin
 	if (!pointsF)
 		return OutOfMemory;
 
-	return GdipDrawImagePointsRect (graphics, image, pointsF, count, srcx, srcy, srcwidth, srcheight, 
+	GpStatus status = GdipDrawImagePointsRect(graphics, image, pointsF, count, srcx, srcy, srcwidth, srcheight,
 		srcUnit, imageAttributes, callback, callbackData);
+	GdipFree(pointsF);
+	return status;
 }
 
 /*
@@ -1073,8 +1091,10 @@ gdip_get_imageformat_from_codec_clsid (CLSID *encoderCLSID)
 	encoders = GdipAlloc (size);
 
 	status = GdipGetImageEncoders (numEncoders, size, encoders);
-	if (status != Ok)
+	if (status != Ok) {
+		GdipFree(encoders);
 		return INVALID;
+	}
 
 	for (cnt = 0, encoder = encoders; cnt < numEncoders; cnt++, encoder++) {
 		if (memcmp (&encoder->Clsid, encoderCLSID, sizeof (GUID)) == 0) {

src/metafile.c
@@ -945,6 +945,8 @@ gdip_metafile_clone (GpMetafile *metafile, GpMetafile **clonedmetafile)
 	}
 
 	mf->base = *base;
+	GdipFree(base);
+	base = NULL;
 
 	memcpy (&mf->metafile_header, &metafile->metafile_header, sizeof (MetafileHeader));
 	if (metafile->length > 0) {

src/pngcodec.c
@@ -451,6 +451,7 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 		result = gdip_bitmap_new_with_frame (&gdip_image_frameDimension_page_guid, TRUE);
 		if (!result) {
 			status = OutOfMemory;
+			GdipFree(palette);
 			goto error;
 		}
 

src/region.c
@@ -866,7 +866,9 @@ gdip_combine_exclude (GpRegion *region, GpRectF *rtrg, int cntt)
 			if (alltrgrects) {
 				GdipFree (alltrgrects);
 			}
-
+			if (allsrcrects) {
+				GdipFree(allsrcrects);
+			}
 			return status;
 		}
 	}
@@ -2106,7 +2108,7 @@ GpStatus WINGDIPAPI
 GdipGetRegionScansI (GpRegion *region, GpRect *rects, INT *count, GpMatrix *matrix)
 {
 	GpStatus status;
-	GpRectF *rectsF;
+	GpRectF *rectsF = NULL;
 	UINT scansCount;
 
 	if (!region || !count || !matrix)
@@ -2125,14 +2127,20 @@ GdipGetRegionScansI (GpRegion *region, GpRect *rects, INT *count, GpMatrix *matr
 	}
 
 	status = GdipGetRegionScans (region, rectsF, count, matrix);
-	if (status != Ok)
+	if (status != Ok) {
+		if (rectsF)
+			free(rectsF);
 		return status;
+	}
 		
 	if (rects) {
 		for (int i = 0; i < scansCount; i++)
 			gdip_Rect_from_RectF (&rectsF[i], &rects[i]);
 	}
 
+	if (rectsF)
+		free(rectsF);
+	
 	return Ok;
 }
 

commit 9e270a1fa1cb2fcf565509c2a3870d8640b2ae79

Fix "double free or corruption" failure in testbmpcodec test
Introduced by ac7547aa763b30f883be3eb49ea7755c8b8d3a31, looks like pixels is already freed.

(cherry picked from commit 33cfae7db95eb69e54c1a3d7c35fefa72f61ef92)

src/bmpcodec.c
@@ -1197,7 +1197,6 @@ gdip_read_bmp_image (void *pointer, GpImage **image, ImageSource source)
 			status = gdip_read_bmp_indexed (pointer, pixels, upsidedown, originalFormat, originalStride, result->active_bitmap->width, result->active_bitmap->height, source);
 			if (status != Ok) {
 				gdip_bitmap_dispose (result);
-				GdipFree(pixels);
 				return status;
 			}
 		}
@@ -1205,7 +1204,6 @@ gdip_read_bmp_image (void *pointer, GpImage **image, ImageSource source)
 		status = gdip_read_bmp_scans (pointer, pixels, upsidedown, originalFormat, originalStride, result->active_bitmap->stride, result->active_bitmap->width, result->active_bitmap->height, source);
 		if (status != Ok) {
 			gdip_bitmap_dispose (result);
-			GdipFree(pixels);
 			return status;
 		}
 	}